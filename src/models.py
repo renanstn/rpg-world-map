d2bd0e437608fdc900efe7cb6bdc8411929c4670
Renan <renan_ota@hotmail.com>
2024-11-04 16:52:15 -0300

    Add relation bewteen map and point

--- src/models.py
+++ src/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import List
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import db
 
@@ -9,9 +12,14 @@ class Map(db.Model):
     map_id: Mapped[str] = mapped_column(nullable=False)
     bucket_path: Mapped[str] = mapped_column(nullable=False)
 
+    points: Mapped[List["Point"]] = relationship(back_populates="map_")
+
 
 class Point(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    map_id: Mapped[int] = mapped_column(ForeignKey("map.id"))
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=True)
     icon_path: Mapped[str] = mapped_column(nullable=True)
+
+    map_: Mapped["Map"] = relationship(back_populates="points")
